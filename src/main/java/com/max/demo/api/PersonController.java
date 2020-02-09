60563c0b2411353f279d0bab839c74c2774d4aee
MAX FELDMAN <maxfeldman99@gmail.com>
2020-02-09 22:52:44 +0200

    API Updated

--- src/main/java/com/max/demo/api/PersonController.java
+++ src/main/java/com/max/demo/api/PersonController.java
@@ -3,10 +3,12 @@ package com.max.demo.api;
 import com.max.demo.model.Person;
 import com.max.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.UUID;
 
 
 @RequestMapping("api/v1/person")
@@ -24,5 +26,26 @@ public class PersonController {
     public void addPerson(@RequestBody Person person){ // requestBody to insert rest body inside
         personService.addPerson(person);
 
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople(){
+        return personService.getAllPeople();
+    }
+
+    @GetMapping(path = "{id}") // taking the id and turning into UUID
+    public Person getPersonById(@PathVariable("id") UUID id){
+        return personService.getPersonById(id)
+                .orElse(null);
+
+    }
+    @DeleteMapping(path = "{id}") // passing the id down to the service
+    public void deletePersonById(@PathVariable("id") UUID id){
+        personService.deletePerson(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updatePerson(@PathVariable("id") UUID id,@Valid @NonNull @RequestBody Person personToUpdate){ // valid will check the restriction inside person model
+        personService.updatePerson(id,personToUpdate);
     }
 }
--- src/main/java/com/max/demo/dao/fakePersonDataAccessService.java
+++ src/main/java/com/max/demo/dao/fakePersonDataAccessService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository("fakeDao")
@@ -17,5 +18,42 @@ public class fakePersonDataAccessService implements PersonDao {
         DB.add(new Person(id,person.getName()));
 
         return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return DB.stream()
+                .filter(person -> person.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        Optional<Person> personMaybe = selectPersonById(id);
+        if(personMaybe.isEmpty()){
+            return 0;
+        }
+        DB.remove(personMaybe.get());
+        return 1;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person update) {
+        return selectPersonById(id)
+                .map(person-> {
+                    int indexOfPersonToUpdate = DB.indexOf(person); // we want the person that we just found
+                    if (indexOfPersonToUpdate >= 0) {
+                     DB.set(indexOfPersonToUpdate,new Person(id,update.getName()));
+                     return 1;
+                    }
+                    return 0;
+
+                })
+                .orElse(0); // because we need to return an integer
     }
 }
